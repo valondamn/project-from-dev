e617cca69ceb1b6d3fd243df2892eaf262ce29e5
aruym <190103062@stu.sdu.edu.kz>
2024-09-26 01:25:22 +0500

    changed server.js

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const path = require("path");
 
 const app = express();
 
-// Указываем Express использовать статические файлы из папки dist
+// Отключаем принудительный редирект на HTTPS
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    return res.redirect('http://' + req.headers.host + req.url);
+  }
+  next();
+});
+
+// Настройка для раздачи статических файлов
 app.use(express.static(__dirname + '/dist/materuak'));
 
-// Перенаправляем все запросы на index.html, чтобы Angular мог обрабатывать маршруты
+// Обработка всех маршрутов
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/materuak/index.html'));
 });
 
-// Указываем порт, который использует Heroku или локальный порт для разработки
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
