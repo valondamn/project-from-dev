f69356e7908550edd10e2271d0c047bcc1335acf
aruym <190103062@stu.sdu.edu.kz>
2024-10-06 12:22:08 +0500

    added failed modal

--- src/app/shared/components/benefits-cards/benefits-cards.component.ts
+++ src/app/shared/components/benefits-cards/benefits-cards.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {BenefitsTop, Cards, User} from '../../../layout/main/main.interface';
 import {SvgIconComponent} from 'angular-svg-icon';
 import {PrimaryButtonComponent} from '../buttons/primary-button/primary-button.component';
@@ -26,7 +26,7 @@ import {environment} from "../../../../enviroments/environment";
   templateUrl: './benefits-cards.component.html',
   styleUrl: './benefits-cards.component.scss',
 })
-export class BenefitsCardsComponent {
+export class BenefitsCardsComponent implements OnInit {
   @Input() isLoading: boolean = true;
   @Input() cards!: Cards[];
   @Input() currentCards!: User;
@@ -34,32 +34,68 @@ export class BenefitsCardsComponent {
   @Input() deadLine: boolean = false;
   @Input() isColumnCard: boolean = true;
   @Input() isLinkCard: boolean = false;
-  public url = environment.serverURL; // Base URL
+  @Input() fontWeight: 'thin' | 'normal' = 'thin';
+  @Input() type: 'catalog' | 'main' = 'main'
+  public url = environment.serverURL;
+  userProfile$ = this.loginService;
 
   public currentCard!: Cards;
   public modalVisible: boolean = false;
   public purchaseSuccessModalVisible: boolean = false;
+  public purchaseFailedModalVisible: boolean = false;
+  private userProfile!: User; // Добавляем свойство для хранения профиля пользователя
 
-  constructor(private benefitsService: BenefitsService,
-              public loginService: LoginService) {
+  constructor(
+    private benefitsService: BenefitsService,
+    private loginService: LoginService
+  ) {
+  }
+
+  ngOnInit() {
+    // Подписываемся на профиль пользователя
+    this.loginService.getProfile().subscribe((profile) => {
+      this.userProfile = profile;
+    });
   }
 
   public confirmPurchase() {
-    console.log('Покупка льготы с ID:', this.currentCard.id);
+    console.log('Покупка льготы с ID:', this.currentCard?.id);
 
-    this.benefitsService.purchaseBenefit(this.currentCard.id).subscribe({
-      next: () => {
-        this.modalVisible = false;
-        this.purchaseSuccessModalVisible = true;
+    if (!this.userProfile) {
+      console.error('Профиль пользователя не загружен');
+      return;
+    }
 
-        // Обновляем профиль пользователя после успешной покупки
-        this.loginService.fetchUserProfile().subscribe();
-      },
-      error: (error) => {
-        console.error('Ошибка при покупке льготы', error);
-        // Обработка ошибок
-      }
-    });
+    if (!this.currentCard) {
+      console.error('Текущая карта не определена');
+      return;
+    }
+
+    if (this.currentCard.cost === undefined || this.currentCard.cost === null) {
+      console.error('Стоимость текущей карты не определена');
+      return;
+    }
+
+    if (this.userProfile.coins < this.currentCard.cost) {
+      // Если коинов недостаточно, показываем модальное окно с сообщением
+      this.modalVisible = false;
+      this.purchaseFailedModalVisible = true;
+    } else {
+      // Если коинов достаточно, выполняем покупку
+      this.benefitsService.purchaseBenefit(this.currentCard.id).subscribe({
+        next: () => {
+          this.modalVisible = false;
+          this.purchaseSuccessModalVisible = true;
+
+          // Обновляем профиль пользователя после успешной покупки
+          this.loginService.fetchUserProfile().subscribe();
+        },
+        error: (error) => {
+          console.error('Ошибка при покупке льготы', error);
+          // Обработка ошибок
+        },
+      });
+    }
   }
 
 
@@ -70,6 +106,7 @@ export class BenefitsCardsComponent {
 
   public closeSuccessModal() {
     this.purchaseSuccessModalVisible = false;
+    this.purchaseFailedModalVisible = false;
   }
 
   public closeConfirmPurchase() {
